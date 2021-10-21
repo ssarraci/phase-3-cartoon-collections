d4278568e4374ff83b0aab67c9010bbbe370bc26
Storm <storm.sarracino01@gmail.com>
2021-10-21 15:47:15 -0500

    commit tree 9ebc3b05539895af8c7633e538946eedbef18644

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, number|
+    puts "#{number + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map {|planeteer|
+  planeteer.capitalize + '!'  
+}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(call)# code an argument here
   # Your code here
+  if call.length > 4
+    false
+  else
+    true
+  end
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |is_cheese|
+    cheese_types.include?(is_cheese)
+  end
 end
